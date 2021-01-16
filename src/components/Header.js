6d25e9bd777a3e06285f00a8872032ea6c51de4c
kshitijkhot <khotd.ug19.cs@nitp.ac.in>
2021-01-16 15:10:30 +0530

    some header is added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 
-import React from "react" ; 
+import React from "react";
 
-const Header = () =>{
+const Header = () => {
     return (
-         <h1> </h1>
+        <div className="jumbotron">
+            <h2 className="display-4"><span class="material-icons logo ">
+                local_dining
+         </span>  Foodstacks Recipes</h2>
+
+            <div class="input-group mx-auto w-50">
+                <input type="text" class="form-control" placeholder="Enter Recipe or food name " aria-label="Search here" aria-describedby="basic-addon2" />
+                <div class="input-group-append">
+                    <span class="input-group-text" id="basic-addon2"><button className="btn btn-primary">Search</button></span>
+                </div>
+            </div>
+            </div>
     )
 }
 
